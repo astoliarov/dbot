2a85283bf9e0ae0e88bed9afd200c4bd50a69770
Aliaksei Staliarou <a_staliarou@flo.health>
2022-05-01 17:36:27 +0300

    moved from pip to poetry; updated deps; updated github pipeline

--- app/config/environ.py
+++ app/config/environ.py
@@ -10,9 +10,9 @@ REDIS_URL = env.str(f"{ENV_PREFIX}_REDIS_URL", "")  # type: ignore
 CHANNEL_CONFIG_PATH = env.str(f"{ENV_PREFIX}_CHANNEL_CONFIG_PATH", "./channel_config/config.json")  # type: ignore
 SENTRY_DSN = env.str(f"{ENV_PREFIX}_SENTRY_DSN", "")  # type: ignore
 
-logging_level = env.str(f"{ENV_PREFIX}_LOGGING_LEVEL", "INFO")  # type: ignore
+logging_level = env.str(f"{ENV_PREFIX}_LOGGING_LEVEL", "DEBUG")  # type: ignore
 if logging_level not in ("INFO", "DEBUG"):
-    logging_level = "INFO"
+    logging_level = "DEBUG"
 
 HEALTHCHECKSIO_WEBHOOK = env.str(f"{ENV_PREFIX}_HEALTHCHECKSIO_WEBHOOK", "")
 
