bb028b118ce32dad2a138e1239c77860b058a330
Alexei Stoliarov <alexei.stoliarov@gmail.com>
2023-11-02 13:55:58 +0200

    refactored monitoring

--- src/dbot/infrastructure/monitoring.py
+++ src/dbot/infrastructure/monitoring.py
@@ -16,19 +16,29 @@ class IMonitoring:
         ...
 
 
-def initialize_monitoring() -> IMonitoring | None:
-    monitoring_config = MonitoringConfiguration()
-
-    if not monitoring_config.healthchecksio_webhook:
-        return None
-
-    return HealthChecksIOMonitoring(monitoring_config.healthchecksio_webhook)
-
-
-class HealthChecksIOMonitoring(IMonitoring):
+class HealthChecksIOClient:
     def __init__(self, webhook: str) -> None:
         self.webhook = webhook
         self.session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=15))
 
-    async def on_job_executed_successfully(self) -> None:
+    async def fire(self) -> None:
         await self.session.get(self.webhook)
+
+
+def initialize_monitoring() -> IMonitoring:
+    monitoring_config = MonitoringConfiguration()
+
+    health_checks_client = None
+    if monitoring_config.healthchecksio_webhook:
+        health_checks_client = HealthChecksIOClient(monitoring_config.healthchecksio_webhook)
+
+    return Monitoring(health_checks_client)
+
+
+class Monitoring(IMonitoring):
+    def __init__(self, health_checks_client: HealthChecksIOClient | None) -> None:
+        self._health_checks_client = health_checks_client
+
+    async def on_job_executed_successfully(self) -> None:
+        if self._health_checks_client:
+            await self._health_checks_client.fire()
--- src/dbot/services.py
+++ src/dbot/services.py
@@ -1,3 +1,7 @@
+import contextlib
+import time
+import typing
+
 import structlog
 
 from dbot.connectors.router import NotificationRouter
@@ -8,45 +12,62 @@ from dbot.repository import Repository
 logger = structlog.getLogger()
 
 
+class ActivityProcessingServiceInstrumentation:
+    def __init__(self, monitoring: IMonitoring) -> None:
+        self._monitoring = monitoring
+
+    @contextlib.asynccontextmanager
+    async def channel_processing(self, channel_id: int) -> typing.AsyncIterator[None]:
+        start = time.monotonic()
+        logger.debug("channel_processing.started", channel_id=channel_id)
+
+        yield
+
+        processing_time = time.monotonic() - start
+        logger.debug("channel_processing.finished", channel_id=channel_id, processing_time=processing_time)
+
+    @contextlib.asynccontextmanager
+    async def channels_processing(self, channels: set[int]) -> typing.AsyncIterator[None]:
+        start = time.monotonic()
+        logger.info("channels_processing.started", ids=channels)
+
+        yield
+
+        processing_time = time.monotonic() - start
+        logger.info("channels_processing.finished", ids=channels, processing_time=processing_time)
+
+        await self._monitoring.on_job_executed_successfully()
+
+
 class ActivityProcessingService:
     def __init__(
         self,
         repository: Repository,
         router: NotificationRouter,
         channels: set[int],
-        monitoring: IMonitoring | None,
+        monitoring: IMonitoring,
     ) -> None:
         self.repository = repository
         self.router = router
         self.channels = channels
-        self.monitoring = monitoring
+        self.instrumentation = ActivityProcessingServiceInstrumentation(monitoring)
 
     def register_client(self, discord_client: IDiscordClient) -> None:
         self.repository.set_discord_client(discord_client)
 
-    async def on_proces_finish(self) -> None:
-        if self.monitoring:
-            await self.monitoring.on_job_executed_successfully()
-
     async def process(self) -> None:
-        logger.info("channels_processing.started", ids=self.channels)
-        for channel_id in self.channels:
-            await self._process_chanel(channel_id)
-
-        await self.on_proces_finish()
-        logger.info("channels_processing.finished", ids=self.channels)
+        async with self.instrumentation.channels_processing(self.channels):
+            for channel_id in self.channels:
+                await self._process_chanel(channel_id)
 
     async def _process_chanel(self, channel_id: int) -> None:
-        logger.debug("channel_processing.started", channel_id=channel_id)
+        async with self.instrumentation.channel_processing(channel_id):
+            channel = await self.repository.get(channel_id)
+            if channel is None:
+                return
 
-        channel = await self.repository.get(channel_id)
-        if channel is None:
-            return
+            notifications = channel.generate_notifications()
 
-        notifications = channel.generate_notifications()
+            await self.router.send(notifications)
 
-        await self.router.send(notifications)
-
-        await self.repository.save(channel)
-
-        logger.debug("channel_processing.finished", channel_id=channel_id)
+            await self.repository.save(channel)
